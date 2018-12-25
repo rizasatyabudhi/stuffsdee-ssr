a9d87fdcca2f45cc5d8a43df219e3a43d5170324
riza.satyabudhi <riza.satyabudhi@aux.go-jek.com>
2018-12-25 22:06:21 +0700

    init

---
+++ out/_next/static/webpack/static/development/pages/index.js.cef70bbc6b867d0d0de2.hot-update.js
@@ -0,0 +1,433 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return _default; });
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! next/head */ "./node_modules/next/head.js");
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(next_head__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+var _jsxFileName = "/Users/rizasatyabudhi/Documents/Bening/stuffsdee-ssr/pages/index.js";
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+
+
+
+var _default =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(_default, _React$Component);
+
+  function _default() {
+    _classCallCheck(this, _default);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(_default).apply(this, arguments));
+  }
+
+  _createClass(_default, [{
+    key: "render",
+    value: function render() {
+      return react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 8
+        },
+        __self: this
+      }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(next_head__WEBPACK_IMPORTED_MODULE_0___default.a, {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 9
+        },
+        __self: this
+      }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("title", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 10
+        },
+        __self: this
+      }, "This page has a title \uD83E\uDD14"), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("meta", {
+        charSet: "utf-8",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 11
+        },
+        __self: this
+      }), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("meta", {
+        name: "viewport",
+        content: "initial-scale=1.0, width=device-width",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 12
+        },
+        __self: this
+      }), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("meta", {
+        property: "og:title",
+        content: "Penyedia Jasa Layanan Gaya Hidup On Demand | GO-LIFE",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 13
+        },
+        __self: this
+      }), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("meta", {
+        property: "og:url",
+        content: "https://www.go-life.co.id/",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 14
+        },
+        __self: this
+      }), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("meta", {
+        property: "og:description",
+        content: "Aplikasi penyedia jasa layanan gaya hidup dengan 1 Juta pengguna di 16 kota. Temukan layanan pijat, kebersihan, mekanik, kecantikan, laundry, dan lainnya.",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 15
+        },
+        __self: this
+      })), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", {
+        className: "App",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 17
+        },
+        __self: this
+      }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("header", {
+        className: "App-header",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 18
+        },
+        __self: this
+      }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("img", {
+        style: {
+          marginTop: 50
+        },
+        src: "https://i.pinimg.com/564x/83/23/66/832366f69c087178414e994299c53a66.jpg",
+        className: "App-logo",
+        alt: "logo",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 20
+        },
+        __self: this
+      }), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("h1", {
+        style: {
+          marginBottom: 0
+        },
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 21
+        },
+        __self: this
+      }, "Stuffsdee"), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("p", {
+        style: {
+          fontSize: '30px',
+          marginTop: '50px',
+          marginBottom: '20px'
+        },
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 22
+        },
+        __self: this
+      }, "Our Collections"), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("button", {
+        type: "button",
+        class: "btn btn-primary btn-lg",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 25
+        },
+        __self: this
+      }, "Large button"), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", {
+        className: "image-wrapper row",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 27
+        },
+        __self: this
+      }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", {
+        className: "col",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 28
+        },
+        __self: this
+      }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("img", {
+        className: "image",
+        src: "https://go-life-public-integration.s3.ap-southeast-1.amazonaws.com/9965bae3-a0a0-40c1-a3dc-5a9beb4a2a32/ScreenShot2018-12-20at11.26.58.png",
+        alt: "",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 29
+        },
+        __self: this
+      }), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("p", {
+        className: "harga",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 30
+        },
+        __self: this
+      }, "IDR 60,000")), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", {
+        className: "col",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 32
+        },
+        __self: this
+      }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("img", {
+        className: "image",
+        src: "https://go-life-public-integration.s3.ap-southeast-1.amazonaws.com/02e7a873-ab9f-4191-95e7-f5a89d37d27d/ScreenShot2018-12-20at11.27.11.png",
+        alt: "",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 33
+        },
+        __self: this
+      }), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("p", {
+        className: "harga",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 34
+        },
+        __self: this
+      }, "IDR 60,000")), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", {
+        className: "col",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 36
+        },
+        __self: this
+      }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("img", {
+        className: "image",
+        src: "https://go-life-public-integration.s3.ap-southeast-1.amazonaws.com/0aff3e89-13fe-4e8a-8a26-e010e88fd8c1/ScreenShot2018-12-20at11.27.30.png",
+        alt: "",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 37
+        },
+        __self: this
+      }), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("p", {
+        className: "harga",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 38
+        },
+        __self: this
+      }, "IDR 65,000")), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", {
+        className: "col",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 40
+        },
+        __self: this
+      }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("img", {
+        className: "image",
+        src: "https://go-life-public-integration.s3.ap-southeast-1.amazonaws.com/f5e9b355-7e50-4fd2-9112-5198706b6b8f/ScreenShot2018-12-20at11.33.07.png",
+        alt: "",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 41
+        },
+        __self: this
+      }), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("p", {
+        className: "harga",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 42
+        },
+        __self: this
+      }, "IDR 65,000")), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", {
+        className: "col",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 44
+        },
+        __self: this
+      }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("img", {
+        className: "image",
+        src: "https://go-life-public-integration.s3.ap-southeast-1.amazonaws.com/bc5ef59e-2ad1-473c-b515-184779b80723/ScreenShot2018-12-20at11.33.11.png",
+        alt: "",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 45
+        },
+        __self: this
+      }), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("p", {
+        className: "harga",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 46
+        },
+        __self: this
+      }, "IDR 65,000")), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", {
+        className: "col",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 48
+        },
+        __self: this
+      }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("img", {
+        className: "image",
+        src: "https://go-life-public-integration.s3.ap-southeast-1.amazonaws.com/a98d9a63-e80d-4f43-9b8a-8421784161cf/ScreenShot2018-12-20at11.33.36.png",
+        alt: "",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 49
+        },
+        __self: this
+      }), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("p", {
+        className: "harga",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 50
+        },
+        __self: this
+      }, "IDR 50,000")), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", {
+        className: "col",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 52
+        },
+        __self: this
+      }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("img", {
+        className: "image",
+        src: "https://go-life-public-integration.s3.ap-southeast-1.amazonaws.com/6f88278b-56a1-423d-9040-4e676b8d4df5/ScreenShot2018-12-20at11.33.41.png",
+        alt: "",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 53
+        },
+        __self: this
+      }), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("p", {
+        className: "harga",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 54
+        },
+        __self: this
+      }, "IDR 50,000")), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", {
+        className: "col",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 56
+        },
+        __self: this
+      }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("img", {
+        className: "image",
+        src: "https://go-life-public-integration.s3.ap-southeast-1.amazonaws.com/f17bc7bc-9db5-418d-86f5-7489af87a777/ScreenShot2018-12-20at11.33.47.png",
+        alt: "",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 57
+        },
+        __self: this
+      }), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("p", {
+        className: "harga",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 58
+        },
+        __self: this
+      }, "IDR 45,000")), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", {
+        className: "col",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 60
+        },
+        __self: this
+      }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("img", {
+        className: "image",
+        src: "https://go-life-public-integration.s3.ap-southeast-1.amazonaws.com/a8887ec0-9ed7-4439-9eef-b97a812d44d0/ScreenShot2018-12-20at11.33.58.png",
+        alt: "",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 61
+        },
+        __self: this
+      }), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("p", {
+        className: "harga",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 62
+        },
+        __self: this
+      }, "IDR 60,000")), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", {
+        className: "col",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 64
+        },
+        __self: this
+      }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("img", {
+        className: "image",
+        src: "https://go-life-public-integration.s3.ap-southeast-1.amazonaws.com/b4ab46b1-e2a9-4e5e-8b76-edd393334a09/ScreenShot2018-12-20at11.36.26.png",
+        alt: "",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 65
+        },
+        __self: this
+      }), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("p", {
+        className: "harga",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 66
+        },
+        __self: this
+      }, "IDR 60,000"))))));
+    }
+  }]);
+
+  return _default;
+}(react__WEBPACK_IMPORTED_MODULE_1___default.a.Component);
+
+
+    (function (Component, route) {
+      if(!Component) return
+      if (false) {}
+      module.hot.accept()
+      Component.__route = route
+
+      if (module.hot.status() === 'idle') return
+
+      var components = next.router.components
+      for (var r in components) {
+        if (!components.hasOwnProperty(r)) continue
+
+        if (components[r].Component.__route === route) {
+          next.router.update(r, Component)
+        }
+      }
+    })(typeof __webpack_exports__ !== 'undefined' ? __webpack_exports__.default : (module.exports.default || module.exports), "/")
+  
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./../node_modules/webpack/buildin/harmony-module.js */ "./node_modules/webpack/buildin/harmony-module.js")(module)))
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.cef70bbc6b867d0d0de2.hot-update.js.map
---
+++ pages/index.js
@@ -0,0 +1,92 @@
+import Head from 'next/head'
+import React from 'react'
+import '../assets/styles/index.css';
+import '../assets/styles/grid.css';
+
+export default class extends React.Component {
+  render() {
+    return (
+      <div>
+        <Head>
+          <title>This page has a title 🤔</title>
+          <meta charSet='utf-8' />
+          <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+          <meta property='og:title' content='Penyedia Jasa Layanan Gaya Hidup On Demand | GO-LIFE' />
+          <meta property='og:url' content='https://www.go-life.co.id/' />
+          <meta property='og:description' content='Aplikasi penyedia jasa layanan gaya hidup dengan 1 Juta pengguna di 16 kota. Temukan layanan pijat, kebersihan, mekanik, kecantikan, laundry, dan lainnya.' />
+        </Head>
+        <div className="App">
+        <header className="App-header">
+
+          <img style={{marginTop:50}} src="https://i.pinimg.com/564x/83/23/66/832366f69c087178414e994299c53a66.jpg" className="App-logo" alt="logo" />
+          <h1 style={{marginBottom:0}}>Stuffsdee</h1>
+          <p style={{fontSize:'30px',marginTop:'50px',marginBottom:'20px'}}>
+            Our Collections
+          </p>
+            <div className="image-wrapper row">
+              <div className="col">
+                <img className="image" src="https://go-life-public-integration.s3.ap-southeast-1.amazonaws.com/9965bae3-a0a0-40c1-a3dc-5a9beb4a2a32/ScreenShot2018-12-20at11.26.58.png" alt=""/>
+                <p className="harga">IDR 60,000</p>
+              </div>
+              <div className="col">
+                <img className="image" src="https://go-life-public-integration.s3.ap-southeast-1.amazonaws.com/02e7a873-ab9f-4191-95e7-f5a89d37d27d/ScreenShot2018-12-20at11.27.11.png" alt=""/>
+                <p className="harga">IDR 60,000</p>
+              </div>
+              <div className="col">
+                <img className="image" src="https://go-life-public-integration.s3.ap-southeast-1.amazonaws.com/0aff3e89-13fe-4e8a-8a26-e010e88fd8c1/ScreenShot2018-12-20at11.27.30.png" alt=""/>
+                <p className="harga">IDR 65,000</p>
+              </div>
+              <div className="col">
+                <img className="image" src="https://go-life-public-integration.s3.ap-southeast-1.amazonaws.com/f5e9b355-7e50-4fd2-9112-5198706b6b8f/ScreenShot2018-12-20at11.33.07.png" alt=""/>
+                <p className="harga">IDR 65,000</p>
+              </div>
+              <div className="col">
+                <img className="image" src="https://go-life-public-integration.s3.ap-southeast-1.amazonaws.com/bc5ef59e-2ad1-473c-b515-184779b80723/ScreenShot2018-12-20at11.33.11.png" alt=""/>
+                <p className="harga">IDR 65,000</p>
+              </div>
+              <div className="col">
+                <img className="image" src="https://go-life-public-integration.s3.ap-southeast-1.amazonaws.com/a98d9a63-e80d-4f43-9b8a-8421784161cf/ScreenShot2018-12-20at11.33.36.png" alt=""/>
+                <p className="harga">IDR 50,000</p>
+              </div>
+              <div className="col">
+                <img className="image" src="https://go-life-public-integration.s3.ap-southeast-1.amazonaws.com/6f88278b-56a1-423d-9040-4e676b8d4df5/ScreenShot2018-12-20at11.33.41.png" alt=""/>
+                <p className="harga">IDR 50,000</p>
+              </div>
+              <div className="col">
+                <img className="image" src="https://go-life-public-integration.s3.ap-southeast-1.amazonaws.com/f17bc7bc-9db5-418d-86f5-7489af87a777/ScreenShot2018-12-20at11.33.47.png" alt=""/>
+                <p className="harga">IDR 45,000</p>
+              </div>
+              <div className="col">
+                <img className="image" src="https://go-life-public-integration.s3.ap-southeast-1.amazonaws.com/a8887ec0-9ed7-4439-9eef-b97a812d44d0/ScreenShot2018-12-20at11.33.58.png" alt=""/>
+                <p className="harga">IDR 60,000</p>
+              </div>
+              <div className="col">
+                <img className="image" src="https://go-life-public-integration.s3.ap-southeast-1.amazonaws.com/b4ab46b1-e2a9-4e5e-8b76-edd393334a09/ScreenShot2018-12-20at11.36.26.png" alt=""/>
+                <p className="harga">IDR 60,000</p>
+              </div>
+            </div>
+{/*
+  <div style={{marginTop:'50px'}}>
+  <a href="https://www.instagram.com/stuffsdee/" target="_blank" >
+  <img src={instagram} alt="social-logo" className="social-logo" />
+  </a>
+  <a href="https://twitter.com/DeeStuffs" target="_blank" >
+  <img src={twitter} alt="social-logo" className="social-logo" />
+  </a>
+  <a href="https://www.facebook.com/stuffs.dee.1" target="_blank" >
+  <img src={facebook} alt="social-logo" className="social-logo" />
+  </a>
+
+  <p style={{fontSize:'25px',marginTop:'10px',marginBottom:'20px'}}>
+  or message via Whatsapp 08788396675
+  </p>
+
+  </div>
+*/}
+
+        </header>
+      </div>
+      </div>
+    )
+  }
+}
